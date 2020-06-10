e390caa53de0d73bd515a1129db6bbe0f16afc35
Enrico Bartz <enrico.bartz@sva.de>
2020-06-10 08:48:02 +0200

    make sure to add dns records

---
+++ create-dns.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+hcloud server list | grep master | grep -v ultra | while read -r line ; do
+  IP=$(echo $line | awk '{print $4}')
+  ID=$(echo $line | awk '{print $2}' | tr -dc '0-9')
+  cfcli add -t A rancher-${ID} $IP
+  cfcli add -t A '*'.rancher-${ID} $IP
+done
